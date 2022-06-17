9bbe19bf857c33a10f7b57454322e4cb9e5c28a2
Cnjerii <christinenjeri162@gmail.com>
2022-06-17 18:24:32 +0300

    commit

--- app/models.py
+++ app/models.py
@@ -6,7 +6,47 @@ class Customer(models.Model):
 	name = models.CharField(max_length=200, null=True)
 	phone = models.CharField(max_length=200, null=True)
 	email = models.CharField(max_length=200, null=True)
-	date_added =models.DateTimeField(auto_now_add=True, null=True)
+	date_added = models.DateTimeField(auto_now_add=True, null=True)
 
 	def __str__(self):
 		return self.name
+
+
+
+class Tag(models.Model):
+	name = models.CharField(max_length=200, null=True)
+
+	def __str__(self):
+		return self.name
+
+class Product(models.Model):
+	CATEGORY = (
+		('Lipgloss', 'Lipgloss'),
+		('Lipscrub', 'Lipscrub'),
+		)
+
+	name = models.CharField(max_length=200, null=True)
+	price = models.FloatField(null=True)
+	category = models.CharField(max_length=200, null=True, choices=CATEGORY)
+	description = models.CharField(max_length=200, null=True, blank=True)
+	date_added = models.DateTimeField(auto_now_add=True, null=True)
+	tags = models.ManyToManyField(Tag)
+
+	def __str__(self):
+		return self.name
+
+
+
+
+class order(models.Model):
+	STATUS = (
+		('Pending', 'Pending'),
+		('Out for delivery', 'Out for delivery'),
+		('Delivered', 'delivered'),
+		)
+
+	customer = models.ForeignKey(Customer, null=True, on_delete=models.SET_NULL)
+	product = models.ForeignKey(Product, null=True, on_delete=models.SET_NULL)
+	date_created = models.DateTimeField(auto_now_add=True, null=True)
+	status = models.CharField(max_length=200, null=True, choices=STATUS)
+	
